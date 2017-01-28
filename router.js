ac9f42890491b6382e8e33440b18f7735db69aa3
Noam Hacker <nhacker95@gmail.com>
2017-01-28 19:39:48 +0000

    basic view functionality

--- router.js
+++ router.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const profile = require('./views/profile');
+
 
 // Ex: GET - http://localhost:3000/
 router.get('/', (req, res) => {
 	res.send('Hello world!');
+});
+
+router.get('/profile', (req, res) => {
+    const a = req.query.atest;
+    const b = req.query.btest;
+	res.send(profile(a + " " + b));
 });
 
 
---
+++ views/profile.js
@@ -0,0 +1,9 @@
+module.exports = function(testvar) {
+	return (
+
+`
+<h1>hello world ${testvar}</h1>
+<input type="text"></input>
+`
+
+)};
