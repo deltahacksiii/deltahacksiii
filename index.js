97567e7d324fbd82f82198ec498976fa460e5dc8
Nathan Seit <nathan888999@gmail.com>
2017-01-29 00:18:22 +0000

    I maka a form

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const bodyParser = require('body-parser');
 const router = require('./router');
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 app.use(router);
 
 app.listen(port, () => {
 	console.log(`Server running on http://localhost:${port}`);
+	console.log('Hello world!');
+	
+	
 });
